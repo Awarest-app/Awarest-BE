8c621d267fd0a26a07c62397176f4715ec049fbd
Minkyu01 <koryum30@gmail.com>
2025-02-04 15:48:33 +0900

    [BE] FEAT: git aws deploy final test1

--- src/authentication/oauth/google/google.strategy.ts
+++ src/authentication/oauth/google/google.strategy.ts
@@ -1,16 +1,20 @@
 // src/authentication/oauth/google/google.strategy.ts
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 
 // googleAuthGuard와 결합되어 사용됨
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
-  constructor() {
+  constructor(private readonly configService: ConfigService) {
     super({
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      // clientID: process.env.GOOGLE_CLIENT_ID,
+      // clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      // callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
+      clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
+      callbackURL: configService.get<string>('GOOGLE_CALLBACK_URL'),
       scope: ['email', 'profile'],
 
       // 1) 항상 계정 선택 창이 뜨도록
