6a68d782d8d57a64e0b9b14985aad07c8f8b9140
Minkyu01 <koryum30@gmail.com>
2025-02-04 13:46:58 +0900

    [BE] FEAT: git aws ecr docker images upload test1

--- scripts/docker-script.sh
+++ scripts/docker-script.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-DOCKER_IMAGE_NAME=my-nest-app
+# DOCKER_IMAGE_NAME=my-nest-app
 
-DOCKER_CONTAINER_NAME=nest-app
+# DOCKER_CONTAINER_NAME=nest-app
 
-docker build -t ${DOCKER_IMAGE_NAME} . # <--- 프로젝트 루트 경로에 대한 상대경로
+# docker build -t ${DOCKER_IMAGE_NAME} . # <--- 프로젝트 루트 경로에 대한 상대경로
 
-docker run -d -p 3001:3000 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
+# docker run -d -p 3001:3000 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
 
 #!/bin/bash
 
 # mkdir -p /home/ec2-user/deploy/zip
 # cd /home/ec2-user/deploy/zip/
 
-# docker-compose down
+docker-compose down
 
-# aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 229262694893.dkr.ecr.us-east-1.amazonaws.com
-# docker-compose pull
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 888577060888.dkr.ecr.us-east-1.amazonaws.com
+docker-compose pull
 
-# docker-compose up -d
+docker-compose up -d
