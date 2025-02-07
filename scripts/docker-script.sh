1b0dfc0971dce7d8159b66ca0ad52d9610117376
Minkyu01 <koryum30@gmail.com>
2025-02-07 16:58:37 +0900

    [BE] FEAT: aws env 테스트7

--- scripts/docker-script.sh
+++ scripts/docker-script.sh
@@ -13,12 +13,10 @@
 # mkdir -p /home/ec2-user/deploy/zip
 # cd /home/ec2-user/deploy/zip/
 
-#!/bin/sh
-
-docker-compose -f /home/ec2-user/my-nest-app/docker/docker-compose.yml down
+docker-compose -f docker/docker-compose.prod.yml down
 
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 888577060888.dkr.ecr.us-east-1.amazonaws.com
 
-docker-compose -f /home/ec2-user/my-nest-app/docker/docker-compose.yml pull
+docker-compose -f docker/docker-compose.prod.yml pull
 
-docker-compose -f /home/ec2-user/my-nest-app/docker/docker-compose.yml up -d
+docker-compose -f docker/docker-compose.prod.yml up -d
