a604d6b45f348193a2b779369c3d961bd68e43bb
Minkyu01 <koryum30@gmail.com>
2025-02-09 15:00:42 +0900

    [BE] FIX: jwt logic admin 9 loge add

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['warn', 'error'], // 모든 로그 레벨 활성화
+    logger: ['log', 'error', 'warn', 'debug'],
   });
 
   app.enableCors({
